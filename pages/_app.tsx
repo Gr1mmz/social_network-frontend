2b3f543406470f57e40c1bdeb9f5896d8ec23c41
Mikhail <admin@kraevskiy-web.ru>
2022-06-09 20:17:57 +0800

    add libraries and create login and register page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import createEmotionCache from '../src/createEmotionCache';
+import {CssBaseline} from '@mui/material';
+import {theme} from '../src/theme';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+};
+
+function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 
 export default MyApp
---
+++ src/theme.ts
@@ -0,0 +1,92 @@
+import {createTheme, ThemeOptions} from "@mui/material/styles";
+
+const themeOptions: ThemeOptions = {
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#2a2143',
+    },
+    secondary: {
+      main: '#de3a72',
+    },
+    background: {
+      default: '#201c2c',
+      paper: '#2a2143',
+    },
+  },
+  components: {
+    MuiSwitch: {
+      styleOverrides: {
+        root: {
+          width: 42,
+          height: 26,
+          padding: 0,
+          margin: 8,
+        },
+        thumb: {
+          width: 24,
+          height: 24,
+        },
+        track: {
+          borderRadius: 13,
+          border: '1px solid #bdbdbd',
+          backgroundColor: '#fafafa',
+          opacity: 1,
+          transition: 'background-color 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+        },
+      },
+    },
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          h1: 'h1',
+          h2: 'h2',
+          h3: 'h3',
+          h4: 'h4',
+          h5: 'h5',
+          h6: 'h6',
+          subtitle1: 'div',
+          subtitle2: 'div',
+          body1: 'div',
+          body2: 'span',
+        },
+      },
+      styleOverrides: {
+        root: {
+          fontWeight: 'bold'
+        }
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          '&.Mui-focused': {
+            'color': '#de3a72'
+          }
+        }
+      }
+    },
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          color: '#de3a72',
+          fontWeight: 400
+        }
+      }
+    },
+    MuiFilledInput: {
+      styleOverrides: {
+        root: {
+          ':before': {
+            borderColor: 'transparent'
+          },
+          ':after': {
+            borderColor: '#de3a72'
+          }
+        }
+      }
+    }
+  },
+};
+
+export const theme = createTheme(themeOptions);
