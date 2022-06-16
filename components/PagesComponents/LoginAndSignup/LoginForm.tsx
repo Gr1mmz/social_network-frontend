f9d157c8601413525d6fb70e1de38e9dacb7bef5
Mikhail <admin@kraevskiy-web.ru>
2022-06-16 14:25:58 +0800

    add login functionality

--- components/PagesComponents/LoginAndSignup/LoginForm.tsx
+++ components/PagesComponents/LoginAndSignup/LoginForm.tsx
@@ -1,22 +1,59 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import Parse from 'parse';
 import {Box, Button, Link, Stack, TextField, Typography} from "@mui/material";
 import NextLink from "next/link";
 import {Formik} from "formik";
 import * as yup from "yup";
+import {useRouter} from 'next/router';
 
-const LoginForm = () => {
+const LoginForm: React.FC<{}> = (): React.ReactElement => {
+  const router = useRouter();
+  const [currentUser, setCurrentUser] = useState<Parse.Object | undefined>();
+
+  const ref = useRef(null);
+
+  const getCurrentUser = async function (): Promise<Parse.User | undefined> {
+    const currentUser: (Parse.User | undefined) = await Parse.User.current();
+    // Update state variable holding current user
+    if(currentUser) {
+      setCurrentUser(currentUser);
+      return currentUser;
+    }
+  };
+
+  const doUserLogIn = async function (actions: any): Promise<boolean> {
+    // @ts-ignore
+    const {username: usernameValue, password: passwordValue} = ref?.current?.values;
+
+    try {
+      const loggedInUser: Parse.User = await Parse.User.logIn(usernameValue, passwordValue);
+      console.log(`Success! User ${loggedInUser.get('username')} has successfully signed in!`,);
+      // @ts-ignore
+      const currentUser: Parse.User = await Parse.User.current();
+      if (loggedInUser === currentUser) {
+        actions.resetForm();
+        await router.push('/');
+      }
+      await getCurrentUser();
+      return true;
+    } catch (error: any) {
+      console.log(`Error! ${error.message}`);
+      return false;
+    }
+  };
+
   const validationSchema = yup.object().shape({
     username: yup.string().required('Обязательное поле'),
     password: yup.string().required('Обязательное поле'),
   });
   return (
-    <Formik validateOnBlur validationSchema={validationSchema}
+    <Formik validateOnBlur validationSchema={validationSchema} innerRef={ref}
             initialValues={{
               username: '',
               password: ''
             }}
             onSubmit={(values, actions) => {
-              console.log(values);
+              doUserLogIn(actions);
             }}>
       {({values,
           errors,
